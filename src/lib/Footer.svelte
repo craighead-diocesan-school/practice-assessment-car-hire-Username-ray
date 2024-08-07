9b84ce66d742b9d551ef1adb88b68c56ca960c2f
Username-ray <maedara96@craighead.school.nz>
2024-08-07 16:51:59 +1200

    Create Footer.svelte
    To be able to share the same footer when creating other pages.

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,13 @@
+<footer>
+  <p>&copy; Craighead Diocesan School 2024</p>
+</footer>
+
+<style>
+  footer {
+    font-family: "PT Serif", serif;
+    text-align: center;
+    font-size: medium;
+    background-color: lightsteelblue;
+    padding: 5%;
+  }
+</style>
